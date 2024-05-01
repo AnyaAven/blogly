14869d96ee5430c8d6720d8986e3dffee1009820
Anya Aven <leafeon96@gmail.com>
2024-05-01 18:57:48 -0400

    Starter config

--- app.py
+++ app.py
@@ -2,8 +2,20 @@
 
 import os
 
-from flask import Flask
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import db, dbx, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     "DATABASE_URL", 'postgresql:///blogly')
+
+app.config['SQLALCHEMY_ECHO'] = True
+db.init_app(app)
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "ANYA AND ALICE's SECRET!")
+print("secret key =", app.config['SECRET_KEY'])
+debug = DebugToolbarExtension(app)
+
+
