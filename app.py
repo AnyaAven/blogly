2111176b1f9f7bf7a113d5352403711d45e6ee57
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-05-01 16:20:57 -0700

    copied over route WIP

--- app.py
+++ app.py
@@ -14,8 +14,19 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
 app.config['SQLALCHEMY_ECHO'] = True
 db.init_app(app)
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "ANYA AND ALICE's SECRET!")
+app.config['SECRET_KEY'] = os.environ.get(
+    'SECRET_KEY', "ANYA AND ALICE's SECRET!")
 print("secret key =", app.config['SECRET_KEY'])
 debug = DebugToolbarExtension(app)
 
 
+@app.get("/")
+def homepage():
+    """Return movie list."""
+
+    q_movies = db.select(Movie)
+    movies = dbx(q_movies).scalars().all()
+
+    q_studios = db.select(Studio)
+    studios = dbx(q_studios).scalars().all()
+    return render_template("movies_list.jinja", movies=movies, studios=studios)
