0ab5348fa96924711fbda0d6be976b1fda55e792
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-05-01 17:18:39 -0700

    added user_ids

--- app.py
+++ app.py
@@ -27,9 +27,12 @@ def homepage():
     q_user = db.select(User)
     users = dbx(q_user).scalars().all()
     # ['Alice Chang','A.A', ...]
-
     user_full_names = [u.get_full_name() for u in users]
+
+    user_ids = [u.id for u in users]
+    # [1, 2]
 
     return render_template(
         "user_listing.jinja",
+        user_ids=user_ids,
         user_full_names=user_full_names)
