890e69d33b4b527da3474a4e48178a7761eeb2c6
Anya Aven <leafeon96@gmail.com>
2024-05-01 20:59:49 -0400

    Added route for homepage and to create a new user

--- app.py
+++ app.py
@@ -19,9 +19,17 @@ app.config['SECRET_KEY'] = os.environ.get(
 print("secret key =", app.config['SECRET_KEY'])
 debug = DebugToolbarExtension(app)
 
-
 @app.get("/")
-def homepage():
+def redirect_to_users():
+    """ Redirect to users
+
+    WIP, add more to this later
+    """
+
+    return redirect("/users")
+
+@app.get("/users")
+def display_users_list():
     """Returns to list of users"""
 
     q_user = db.select(User)
@@ -36,3 +44,29 @@ def homepage():
         "user_listing.jinja",
         user_ids=user_ids,
         user_full_names=user_full_names)
+
+
+@app.get("/users/new")
+def display_new_user_form():
+    """ Display new user form """
+
+    return render_template("new_user_form.jinja")
+
+@app.post("/users/new")
+def add_user():
+    """ Add new user to the database and redirect to /users """
+
+    fname = request.form['first_name']
+    lname = request.form['last_name']
+
+    # Use user's image url or the default img url
+    img_url = request.form["image_url"] # TODO: , "https://via.placeholder.com/250"
+
+
+    new_user = User(first_name=fname, last_name=lname, image_url=img_url)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect("/users")
+
